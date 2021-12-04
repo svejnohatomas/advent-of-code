7acd9f70010332d98c4128c71689ac9a1e9e5eb9
Tomas Svejnoha <svejnohatomas@users.noreply.github.com>
2021-12-04 21:34:31 +0000

    Add solution for 2021-12-04

---
+++ SvejnohaTomas.AdventOfCode/src/2021/AdventOfCode.2021.12.04/BingoBoard.cs
@@ -0,0 +1,81 @@
+﻿namespace AdventOfCode._2021._12._04
+{
+    internal class BingoBoard
+    {
+        public BingoBoardCell[,] Cells { get; }
+        public bool Bingo { get; private set; }
+
+        public BingoBoard()
+        {
+            Cells = new BingoBoardCell[5, 5];
+
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    Cells[x, y] = new BingoBoardCell();
+                }
+            }
+        }
+
+        public void Mark(int number)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    if (Cells[x, y].Value == number)
+                    {
+                        Cells[x, y].Marked = true;
+                    }
+                }
+            }
+        }
+
+        public bool IsBingo()
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                bool[] rowMarks = new bool[5];
+                bool[] colMarks = new bool[5];
+
+                for (int col = 0; col < 5; col++)
+                {
+                    rowMarks[col] = Cells[row, col].Marked;
+                    colMarks[col] = Cells[col, row].Marked;
+                }
+
+                if (rowMarks.All(x => x) || colMarks.All(x => x))
+                {
+                    Bingo = true;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int Score(int lastDrawnNumber)
+        {
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (!Cells[i, j].Marked)
+                    {
+                        sum += Cells[i, j].Value;
+                    }
+                }
+            }
+
+            return sum * lastDrawnNumber;
+        }
+
+        internal class BingoBoardCell
+        {
+            public int Value { get; set; }
+            public bool Marked { get; set; }
+        }
+    }
+}
---
+++ SvejnohaTomas.AdventOfCode/src/2021/AdventOfCode.2021.12.04/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode._2021._12._04;
+
+int[] drawNumbers = null!;
+LinkedList<BingoBoard> boards = new LinkedList<BingoBoard>();
+
+using (var sr = new StreamReader("./input.txt"))
+{
+    if (!sr.EndOfStream)
+    {
+        var line = sr.ReadLine();
+        if (line is null)
+            return;
+
+        drawNumbers = line.Split(',').Select(x => int.Parse(x)).ToArray();
+    }
+
+    while(!sr.EndOfStream)
+    {
+        // Discard separating line
+        sr.ReadLine();
+
+        var bingoBoard = new BingoBoard();
+
+        for (int i = 0; i < 5; i++)
+        {
+            var line = sr.ReadLine()?.Trim().Replace("  ", " ");
+            if (line is null)
+                throw new Exception("Input parse error");
+
+            var lineNumbers = line.Split(' ').Select(sr => int.Parse(sr)).ToArray();
+            for (int j = 0; j < lineNumbers.Length; j++)
+            {
+                bingoBoard.Cells[i,j].Value = lineNumbers[j];
+            }
+        }
+
+        boards.AddLast(bingoBoard);
+    }
+}
+
+var scores = new List<int>(boards.Count);
+
+for (int i = 0; i < drawNumbers.Length && boards.Any(x => !x.Bingo); i++)
+{
+    var number = drawNumbers[i];
+
+    foreach (var item in boards)
+    {
+        if (item.Bingo)
+            continue;
+
+        item.Mark(number);
+
+        if (item.IsBingo())
+        {
+            scores.Add(item.Score(number));
+        }
+    }
+}
+
+Console.WriteLine($"" +
+    $"Author: Tomas Svejnoha\n" +
+    $"Date: 2021-12-04\n\n" +
+    $"Part 1:\n" +
+    $"The score of the winning board is: {scores[0]}\n\n" +
+    $"Part 2:\n" +
+    $"The score of the loosing board is: {scores[scores.Count - 1]}\n");
+
+Console.Write("Press enter to exit...");
+Console.ReadLine();
