37eb76156923de3310bbae3ee203cfa4b04c3e9c
Tomas Svejnoha <svejnohatomas@users.noreply.github.com>
2021-12-06 23:23:25 +0000

    Add solution for 2021-12-06 Part 1

---
+++ SvejnohaTomas.AdventOfCode/src/2021/AdventOfCode.2021.12.06/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+LinkedList<byte> linkedList;
+
+using (var sr = new StreamReader("./input.txt"))
+{
+    var line = sr.ReadLine();
+    if (line == null)
+        return;
+
+    linkedList = new LinkedList<byte>(line.Split(',').Select(x => byte.Parse(x)));
+}
+
+int days;
+string input;
+
+do
+{
+    Console.Write("Enter number of days: ");
+    input = (Console.ReadLine() ?? "").Trim();
+} while (!int.TryParse(input, out days));
+
+for (int d = 0; d < days; d++)
+{
+    int numberOfNewLanternfish = 0;
+    int queueCount = linkedList.Count;
+
+    for (int i = 0; i < queueCount; i++)
+    {
+        var item = linkedList.First;
+        linkedList.RemoveFirst();
+
+        if (item.Value <= 0)
+        {
+            // Add new lanternfish
+            numberOfNewLanternfish++;
+
+            // Reset day count
+            linkedList.AddLast(6);
+        }
+        else
+        {
+            linkedList.AddLast(--item.Value);
+        }
+    }
+
+    for (int i = 0; i < numberOfNewLanternfish; i++)
+    {
+        linkedList.AddLast(8);
+    }
+}
+
+Console.WriteLine($"\n" +
+    $"Author: Tomas Svejnoha\n" +
+    $"Date: 2021-12-06\n\n" +
+    $"Number of lanternfish after {days} days: {linkedList.Count}\n\n");
+
+Console.Write("Press enter to exit...");
+Console.ReadLine();
